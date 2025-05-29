90bf4df97ea1420323ad4155c3f070e6ed9ffcbd
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-05-29 23:42:22 +0530

    Working Changes : Project section animated

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ document.addEventListener("DOMContentLoaded", () => {
     if (circle) {
       console.log("Circle pointer element found");
       document.addEventListener("mousemove", (e) => {
-        circle.style.left = e.clientX + "px";
-        circle.style.top = e.clientY + "px";
+        circle.style.left = e.pageX + "px";
+        circle.style.top = e.pageY + "px";
       });
     } else {
       console.error('Element with class "circle-ptr" not found.');
@@ -50,17 +50,58 @@ document.addEventListener("DOMContentLoaded", () => {
   circleMousePointer();
 
   document.querySelectorAll(".elem").forEach(function (elem) {
-    elem.addEventListener("mousemove", function (dets) {
-      console.log("Mouse moved over element");
-
-      let diff = dets.clientY - elem.getBoundingClientRect().top;
-      console.log(diff);
-
-      gsap.to(elem.querySelector("img"), {
+    let img = elem.querySelector("img");
+    let rect = elem.getBoundingClientRect();
+    let projectInfo = elem.querySelector(".project-info");
+    
+    elem.addEventListener("mouseenter", function () {
+      gsap.to(img, {
         opacity: 1,
-        ease: Power1.easeInOut,
-        top: diff,
-        left: dets.clientX,
+        scale: 1,
+        duration: 0.4,
+        ease: "power2.out"
+      });
+      
+      gsap.to(projectInfo, {
+        opacity: 1,
+        y: 0,
+        duration: 0.4,
+        delay: 0.1,
+        ease: "power2.out"
+      });
+    });
+
+    elem.addEventListener("mousemove", function (dets) {
+      let x = dets.clientX - rect.left;
+      let y = dets.clientY - rect.top;
+      
+      // Calculate the movement range (smaller for subtler effect)
+      let moveX = (x - rect.width / 2) * 0.1;
+      let moveY = (y - rect.height / 2) * 0.1;
+      
+      gsap.to(img, {
+        x: moveX,
+        y: moveY,
+        duration: 0.3,
+        ease: "power2.out"
+      });
+    });
+
+    elem.addEventListener("mouseleave", function () {
+      gsap.to(img, {
+        opacity: 0,
+        scale: 0.8,
+        x: 0,
+        y: 0,
+        duration: 0.3,
+        ease: "power2.out"
+      });
+      
+      gsap.to(projectInfo, {
+        opacity: 0,
+        y: 20,
+        duration: 0.3,
+        ease: "power2.out"
       });
     });
   });
